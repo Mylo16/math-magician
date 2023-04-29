8523776547b550eaf874d4fe511ccc3de9227d54
Mylo16 <ericopokuantwi384@gmail.com>
2023-04-29 20:23:01 +0000

    finish fetching quote API

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import Quote from './components/Quote';
 import Calculator from './components/calculator';
 
 function App() {
   return (
     <>
       <Calculator />
+      <Quote />
     </>
   );
 }
