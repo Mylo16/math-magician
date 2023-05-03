01fe352e593d4284a885cc7e817e610a490120c3
Antwi Eric <ANTWI@DESKTOP-92MFH60.(none)>
2023-05-03 23:14:35 +0000

    finish adding different pages

--- src/App.js
+++ src/App.js
@@ -1,12 +1,41 @@
+import {
+  Routes, Route, Link, Outlet,
+} from 'react-router-dom';
 import './App.css';
+import Home from './components/Home';
 import Quote from './components/Quote';
 import Calculator from './components/calculator';
 
 function App() {
+  function Layout() {
+    return (
+      <>
+        <nav>
+          <div className="navbar">
+            <h1 className="nav-logo">Math Magician</h1>
+            <ul className="nav-lists">
+              <li className="nav-list"><Link className="link" to="/">Home</Link></li>
+              <li className="nav-list"><Link className="link" to="/calculator">Calculator</Link></li>
+              <li className="nav-list"><Link className="link" to="/quote">Quote</Link></li>
+            </ul>
+          </div>
+        </nav>
+        <Outlet />
+        <footer>
+          <p>This app is Licensed cc</p>
+        </footer>
+      </>
+    );
+  }
   return (
     <>
-      <Calculator />
-      <Quote />
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="calculator" element={<Calculator />} />
+          <Route path="quote" element={<Quote />} />
+        </Route>
+      </Routes>
     </>
   );
 }
